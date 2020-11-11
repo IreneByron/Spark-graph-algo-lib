78ef44f336e4fd88513b384d5186f92a8ed7d7aa
IreneByron <zhangbingqing7@huawei.com>
2020-11-11 14:19:53 +0800

    fix

--- src/main/scala/org/apache/spark/graphx/lib/ModularityCompute.scala
+++ src/main/scala/org/apache/spark/graphx/lib/ModularityCompute.scala
@@ -5,8 +5,8 @@ import org.apache.spark.rdd.RDD
 object Modularity {
 
   def run(
-    nodes: RDD[(Int, Int)],
-    edges: RDD[(Int, Int, Double)],
+    nodes: RDD[(Long, Long)],
+    edges: RDD[(Long, Long, Double)],
     isDirected: Boolean,
     partitinoNum: Int = 1): Double = {
     0.0
